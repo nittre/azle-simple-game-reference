1c93ef81ff521c94a37ab4ee7942261c20e38445
maemil <youjinlee1997@gmail.com>
2023-10-13 04:11:47 +0900

    v17 -> v18 upgrade

--- simple_game/dfxcmd.bat
+++ simple_game/dfxcmd.bat
@@ -1,8 +1,17 @@
 @echo on
+dfx identity use default
+cd ../token
+dfx canister call token initialize '("CozToken","COZ",10000)'
+dfx canister call token addAdmin '("bd3sg-teaaa-aaaaa-qaaba-cai")'
+dfx canister call token approve '("bd3sg-teaaa-aaaaa-qaaba-cai",1000)'
+dfx canister call token transfer '("icjnw-eeogh-khwwx-k2cpg-ydxxg-2kvmg-frefb-r7ol4-w5z5v-mazip-yae", 1000)'
 dfx identity use second-identity
-dfx canister call simple_game initialize '("b77ix-eeaaa-aaaaa-qaada-cai")'
+dfx canister call token approve '("bd3sg-teaaa-aaaaa-qaaba-cai",1000)'
+dfx identity use default
+cd ../simple_game
+dfx canister call simple_game initialize '("be2us-64aaa-aaaaa-qaabq-cai")'
 dfx canister call simple_game createCharacter '()'
 dfx canister call simple_game createBattle '(100,4)'
 dfx canister call simple_game openedBattles '()'
-dfx identity use third-identity
+dfx identity use second-identity
 dfx canister call simple_game createCharacter '()'
--- simple_game/src/index.ts
+++ simple_game/src/index.ts
@@ -1,112 +1,80 @@
 import {
   nat64,
   Record,
-  $query,
-  $update,
-  ic,
-  match,
   Vec,
-  Service,
-  serviceUpdate,
-  CallResult,
   Principal,
   nat16,
-  serviceQuery,
+  text,
+  bool,
+  Canister,
+  update,
+  query,
+  ic,
+  float64,
 } from "azle";
+
+import TokenCanister from "../../token/src";
 
 const RETRY_RATE = 100;
 
 // 캐릭터 타입 정의
-type Character = Record<{
-  owner: string; // owner's principal address
-  retryCount: nat64; // 배열의 랜덤값을 갱신할 수 있는 횟수
-  battleHistory: Vec<string>; // 배틀 기록 아이디 배열
-}>;
+export const Character = Record({
+  owner: text,
+  retryCount: nat64,
+  battleHistory: Vec(text),
+});
 
-type CharacterInBattle = Record<{
-  owner: string;
-  result: number;
-  approved?: boolean;
-  gameResults?: Vec<number>;
-}>;
+export const CharacterInBattle = Record({
+  owner: text,
+  result: float64,
+  approved: bool,
+  gameResults: Vec(float64),
+});
 
 // 배틀 타입 정의
-type Battle = Record<{
-  id: string; // uuid
-  characters: Vec<Character>; // 참여한 캐릭터들의 주소 배열
-  results: Vec<CharacterInBattle>; // 각 캐릭터의 결과값
-  betAmount: nat64; // 배팅된 토큰 수
-  maxParticipantAmount: nat16; // 최대 참여자 수
-  battleAdmin: string;
-  winner?: CharacterInBattle;
-}>;
+export const Battle = Record({
+  id: text,
+  characters: Vec(Character),
+  betAmount: nat64,
+  maxParticipantAmount: nat16,
+  results: Vec(CharacterInBattle),
+  battleAdmin: text,
+  winner: CharacterInBattle,
+});
 
-type BattleInfo = Record<{
-  id: string;
-  characters: Vec<Character>;
-  betAmount: nat64;
-  maxParticipantAmount: nat16;
-}>;
+export const BattleInfo = Record({
+  id: text,
+  characters: Vec(Character),
+  betAmount: nat64,
+  maxParticipantAmount: nat16,
+  results: Vec(CharacterInBattle),
+  battleAdmin: text,
+});
 
-const characters: Vec<Character> = [];
-const battles: Vec<Battle> = [];
+const characters: Vec<typeof Character> = [];
+const battles: Vec<typeof Battle> = [];
 let tokenOwner = ""; // 토큰 오너 주소
-
-class TokenCanister extends Service {
-  @serviceUpdate
-  burn: (from: string, amount: nat64) => CallResult<boolean>;
-
-  @serviceQuery
-  allowanceFrom: (owner: string) => CallResult<nat64>;
-
-  @serviceUpdate
-  transferFrom: (
-    fromAddress: string,
-    toAddress: string,
-    amount: nat64
-  ) => CallResult<boolean>;
-}
-
-let tokenCanisterInstance: TokenCanister;
+let tokenCanister: typeof TokenCanister;
 
 async function _burn(from: string, amount: nat64): Promise<boolean> {
-  const burnCallResult = await tokenCanisterInstance.burn(from, amount).call();
-
-  const result = match(burnCallResult, {
-    Ok: (value: boolean) => true,
-    Err: (error: string) => {
-      throw new Error("Burn Failed.");
-    },
+  return await ic.call(tokenCanister.burn, {
+    args: [from, amount],
   });
-
-  return result;
 }
 
 async function _allowanceFrom(owner: string): Promise<bigint> {
-  const allowanceCallResult = await tokenCanisterInstance
-    .allowanceFrom(owner)
-    .call();
-
-  return match(allowanceCallResult, {
-    Ok: (result) => result,
-    Err: () => 0n,
+  return await ic.call(tokenCanister.allowanceFrom, {
+    args: [owner],
   });
 }
 
 async function _transferFrom(
-  fromAddress: string,
-  toAddress: string,
+  from: string,
+  to: string,
   amount: nat64
 ): Promise<boolean> {
-  const transferFromResponse = await tokenCanisterInstance
-    .transferFrom(fromAddress, toAddress, amount)
-    .call();
-
-  return match(transferFromResponse, {
-    Ok: () => true,
-    Err: () => {
-      throw new Error("transferFrom Failed");
-    },
+  return await ic.call(tokenCanister.transferFrom, {
+    args: [from, to, amount],
   });
 }
 
@@ -118,12 +86,23 @@ function getCaller(): string {
   return caller;
 }
 
-/*
- * 반환 타입이 nat16, nat32이면 무조건 내림처리 되어서 0이되고,
- * nat64는 bigint라 Math.random()의 반환타입인 Number 타입과 상충됨
- * 반환타입을 Number로 하면 아무 문제도 생기지 않음 (???)
- */
-function getRandomNumber(): number {
+function _getCharacterByOwner(owner: string): typeof Character {
+  const character = characters.find((char) => char.owner === owner);
+  if (!character) {
+    throw new Error("no character found");
+  }
+  return character;
+}
+
+function _getBattleByUuid(uuid: string): typeof Battle {
+  const index = battles.findIndex((battle) => battle.id === uuid);
+  if (index === -1) {
+    throw new Error("no battle found");
+  }
+  return battles[index];
+}
+
+function getRandomNumber(): nat16 {
   return Math.random();
 }
 
@@ -140,239 +119,204 @@ function generateRandomUUID(): string {
   return uuid;
 }
 
-$update;
-export function initialize(tokenCanister: string): boolean {
-  if (tokenOwner.length != 0) {
-    return false;
-  }
-  const caller = getCaller();
-  tokenOwner = caller;
-
-  tokenCanisterInstance = new TokenCanister(Principal.fromText(tokenCanister));
-
-  return true;
-}
-
-$query;
-export function getCharacterByOwner(owner: string): Character {
-  const character = characters.find((char) => char.owner === owner);
-  if (!character) {
-    throw new Error("no character found");
-  }
-  return character;
-}
-
-$update;
-export function createCharacter(): boolean {
-  const newCharacter: Character = {
-    owner: getCaller(),
-    retryCount: 1n,
-    battleHistory: [],
-  };
-
-  characters.push(newCharacter);
-  return true;
-}
-
-/*
-    사용자는 토큰을 소모해 RetryCount를 업그레이드 할 수 있다.
-    사용자는 업그레이드 할 양을 캐니스터에 approve해두어야 한다.
-*/
-$update;
-export async function upgradeRetryCountWithTokens(
-  amount: nat16
-): Promise<boolean> {
-  const caller = getCaller();
-  const character = characters.find((char) => char.owner === caller);
-
-  if (!character) {
-    throw new Error("Character not found");
-  }
-
-  if (amount < RETRY_RATE) {
-    throw new Error(`Amount must be bigger than ${RETRY_RATE}`);
-  }
-
-  // 1. amount를 RETRY_RATE로 나눈 나머지 값은 버리고, 몫만 취한다.
-  // 1-1. 이때, 사용자가 소모할 토큰의 양은 tokenAmountToSpend (amount - 나머지)
-  const tokenAmountToSpend = Math.floor(amount / RETRY_RATE) * RETRY_RATE;
-
-  // 1-2. 업그레이드 될 RetryCount의 양은 retryCountAmountToUpgrade (몫)
-  const retryCountAmountToUpgrade = Math.floor(amount / RETRY_RATE);
-
-  // 2. tokenAmountToSpend만큼의 토큰을 burn 한다.
-  // burn(사용자, amount)
-  const burnCallResult = await _burn(caller, BigInt(tokenAmountToSpend));
-
-  // 3. retryCountAmountToUpgrade만큼 캐릭터를 upgrade한다.
-  if (burnCallResult) {
-    const indexToModify = characters.findIndex(
-      (char) => char.owner == character.owner
-    );
-    if (indexToModify !== -1) {
-      characters[indexToModify].retryCount += BigInt(retryCountAmountToUpgrade);
-    }
+export default Canister({
+  initialize: update([text], bool, (tokenCanisterAddress) => {
+    tokenCanister = TokenCanister(Principal.fromText(tokenCanisterAddress));
     return true;
-  } else {
-    return false;
-  }
-}
+  }),
+  getCharacterByOwner: query([text], Character, (owner) => {
+    return _getCharacterByOwner(owner);
+  }),
+  createCharacter: update([], bool, () => {
+    const newCharacter: typeof Character = {
+      owner: getCaller(),
+      retryCount: 1n,
+      battleHistory: [],
+    };
 
-/* createBattle: 배틀 생성 함수
- * 사용자는 이 함수를 호출해 새로운 배틀을 생성한다.
- * 사용자는 방장이 되어, 배틀을 실행할 권한을 가진다.
- * 배틀을 생성할 때, 배틀에 걸릴 토큰의 수를 정한다.
- * 사용자는 해당 함수를 호출하기 전에 betAmount 이상의 토큰을 컨트랙트에 approve 해두어야 한다.
- */
-$update;
-export async function createBattle(
-  betAmount: nat64,
-  maxParticipantAmount: nat16
-): Promise<boolean> {
-  const caller = getCaller();
-  const callerCharacter = characters.find((char) => char.owner === caller);
+    characters.push(newCharacter);
+    return true;
+  }),
+  upgradeRetryCountWithTokens: update([nat16], bool, async (amount) => {
+    /*
+          사용자는 토큰을 소모해 RetryCount를 업그레이드 할 수 있다.
+          사용자는 업그레이드 할 양을 캐니스터에 approve해두어야 한다.
+      */
+    const caller = getCaller();
+    const character = characters.find((char) => char.owner === caller);
 
-  if (!callerCharacter) {
-    throw new Error("Character not found");
-  }
+    if (!character) {
+      throw new Error("Character not found");
+    }
 
-  // 1. 사용자가 BetAmount만큼 approve 해두었는지 확인
-  const allowance = await _allowanceFrom(caller);
-  if (allowance < betAmount) {
-    throw new Error("Not enough allowance");
-  }
+    if (amount < RETRY_RATE) {
+      throw new Error(`Amount must be bigger than ${RETRY_RATE}`);
+    }
 
-  // 2. 배틀 생성
-  const newBattle: Battle = {
-    id: generateRandomUUID(),
-    characters: [callerCharacter],
-    results: [],
-    betAmount: betAmount,
-    battleAdmin: caller,
-    maxParticipantAmount: maxParticipantAmount,
-    winner: undefined,
-  };
+    // 1. amount를 RETRY_RATE로 나눈 나머지 값은 버리고, 몫만 취한다.
+    // 1-1. 이때, 사용자가 소모할 토큰의 양은 tokenAmountToSpend (amount - 나머지)
+    const tokenAmountToSpend = Math.floor(amount / RETRY_RATE) * RETRY_RATE;
 
-  battles.push(newBattle);
+    // 1-2. 업그레이드 될 RetryCount의 양은 retryCountAmountToUpgrade (몫)
+    const retryCountAmountToUpgrade = Math.floor(amount / RETRY_RATE);
 
-  return true;
-}
+    // 2. tokenAmountToSpend만큼의 토큰을 burn 한다.
+    // burn(사용자, amount)
+    const burnCallResult = await _burn(caller, BigInt(tokenAmountToSpend));
 
-$query;
-export function openedBattles(): Vec<BattleInfo> {
-  return battles
-    .filter((battle) => battle.winner === undefined)
-    .map((battle) => {
-      return {
-        id: battle.id,
-        characters: battle.characters,
-        betAmount: battle.betAmount,
-        maxParticipantAmount: battle.maxParticipantAmount,
+    // 3. retryCountAmountToUpgrade만큼 캐릭터를 upgrade한다.
+    if (burnCallResult) {
+      const indexToModify = characters.findIndex(
+        (char) => char.owner == character.owner
+      );
+      if (indexToModify !== -1) {
+        characters[indexToModify].retryCount += BigInt(
+          retryCountAmountToUpgrade
+        );
+      }
+      return true;
+    } else {
+      return false;
+    }
+  }),
+  createBattle: update(
+    [nat64, nat16],
+    bool,
+    async (betAmount, maxParticipantAmount) => {
+      /* createBattle: 배틀 생성 함수
+       * 사용자는 이 함수를 호출해 새로운 배틀을 생성한다.
+       * 사용자는 방장이 되어, 배틀을 실행할 권한을 가진다.
+       * 배틀을 생성할 때, 배틀에 걸릴 토큰의 수를 정한다.
+       * 사용자는 해당 함수를 호출하기 전에 betAmount 이상의 토큰을 컨트랙트에 approve 해두어야 한다.
+       */
+      const caller = getCaller();
+      const callerCharacter = characters.find((char) => char.owner === caller);
+
+      if (!callerCharacter) {
+        throw new Error("Character not found");
+      }
+
+      // 1. 사용자가 BetAmount만큼 approve 해두었는지 확인
+      const allowance = await _allowanceFrom(caller);
+      if (allowance < betAmount) {
+        throw new Error("Not enough allowance");
+      }
+
+      // 2. 배틀 생성
+      const newBattle: typeof Battle = {
+        id: generateRandomUUID(),
+        characters: [callerCharacter],
+        results: [],
+        betAmount: betAmount,
+        battleAdmin: caller,
+        maxParticipantAmount: maxParticipantAmount,
+        winner: {
+          owner: "0",
+          result: 0,
+          approved: false,
+          gameResults: [],
+        },
       };
-    });
-}
 
-$query;
-export function getBattleByUuid(uuid: string): Battle {
-  const index = battles.findIndex((battle) => battle.id === uuid);
-  if (index === -1) {
-    throw new Error("no battle found");
-  }
-  return battles[index];
-}
+      battles.push(newBattle);
 
-/*
-  입장하는 참여자는 미리 토큰을 approve 해두어야 한다.
-*/
-$update;
-export async function enterBattle(battleId: string): Promise<boolean> {
-  const caller = getCaller();
-  const character = characters.find((char) => char.owner === caller);
+      return true;
+    }
+  ),
+  openedBattles: query([], Vec(Battle), () => {
+    return battles.filter((battle) => battle.winner.owner === "0");
+  }),
+  getBattleByUuid: query([text], Battle, (uuid) => {
+    return _getBattleByUuid(uuid);
+  }),
+  enterBattle: update([text], bool, async (battleId) => {
+    /*
+        입장하는 참여자는 미리 토큰을 approve 해두어야 한다.
+      */
+    const caller = getCaller();
+    const character = characters.find((char) => char.owner === caller);
 
-  if (!character) {
-    throw new Error("Character not found");
-  }
+    if (!character) {
+      throw new Error("Character not found");
+    }
 
-  const battle = getBattleByUuid(battleId);
+    const battle = _getBattleByUuid(battleId);
 
-  if (battle.maxParticipantAmount <= battle.characters.length) {
-    throw new Error("battle is full");
-  }
+    if (battle.maxParticipantAmount <= battle.characters.length) {
+      throw new Error("battle is full");
+    }
 
-  const allowance = await _allowanceFrom(caller);
-
-  if (allowance < battle.betAmount) {
-    throw new Error("Not enough allowance");
-  }
-
-  battle.characters.push(character);
-
-  return true;
-}
-
-/*
-  게임 진행 및 정산
-*/
-$update;
-export async function endBattle(battleId: string): Promise<Battle> {
-  const battle = getBattleByUuid(battleId);
-
-  // 1. endBattle은 방장(battleAdmin)만이 실행할 수 있다. 먼저 방장이 호출했는지 확인한다.
-  const caller = getCaller();
-  if (caller !== battle.battleAdmin) {
-    throw new Error("Only BattleAdmin can end battle");
-  }
-
-  // 2. 참여자들을 돌면서 allowance가 충분한지 확인한다.
-  // 2-1. allowance가 충분하지 않으면 배틀에서 제외
-  for (let character of battle.characters) {
-    const allowance = await _allowanceFrom(character.owner);
+    const allowance = await _allowanceFrom(caller);
 
     if (allowance < battle.betAmount) {
-      battle.results.push({
-        owner: character.owner,
-        result: 0,
-        gameResults: [],
-        approved: false,
-      });
+      throw new Error("Not enough allowance");
     }
-  }
 
-  // 3. 참여자들마다 Math.random을 돌리게 한다. retryCount가 1 이상이면, 카운트 수만큼 돌리고, 가장 1에 근접한 수를 result로 삼는다.
-  // 각 참여자들의 result를 battle.results에 추가한다.
+    battle.characters.push(character);
 
-  for (let characterInBattle of battle.characters) {
-    const character = getCharacterByOwner(characterInBattle.owner);
-    const characterGameResult: Vec<number> = [getRandomNumber()];
-    if (character.retryCount > 1) {
-      for (let i = 1; i < character.retryCount; i++) {
-        characterGameResult.push(getRandomNumber());
+    return true;
+  }),
+
+  endBattle: update([text], Battle, async (battleId) => {
+    /*
+        게임 진행 및 정산
+      */
+    const battle = _getBattleByUuid(battleId);
+
+    // 1. endBattle은 방장(battleAdmin)만이 실행할 수 있다. 먼저 방장이 호출했는지 확인한다.
+    const caller = getCaller();
+    if (caller !== battle.battleAdmin) {
+      throw new Error("Only BattleAdmin can end battle");
+    }
+
+    // 2. 참여자들을 돌면서 allowance가 충분한지 확인한다.
+    // 2-1. allowance가 충분하지 않으면 배틀에서 제외
+
+    // 3. 참여자들마다 Math.random을 돌리게 한다. retryCount가 1 이상이면, 카운트 수만큼 돌리고, 가장 1에 근접한 수를 result로 삼는다.
+    // 각 참여자들의 result를 battle.results에 추가한다.
+
+    for (let character of battle.characters) {
+      const allowance = await _allowanceFrom(character.owner);
+
+      if (allowance < battle.betAmount) {
+        battle.results.push({
+          owner: character.owner,
+          result: 0,
+          gameResults: [],
+          approved: false,
+        });
+      } else {
+        const characterGameResult: Vec<number> = [getRandomNumber()];
+        if (character.retryCount > 1) {
+          for (let i = 1; i < character.retryCount; i++) {
+            characterGameResult.push(getRandomNumber());
+          }
+        }
+        battle.results.push({
+          owner: character.owner,
+          result: Math.max(...characterGameResult),
+          gameResults: characterGameResult,
+          approved: true,
+        });
       }
     }
-    battle.results.push({
-      owner: characterInBattle.owner,
-      result: Math.max(...characterGameResult),
-      gameResults: characterGameResult,
-      approved: true,
+
+    battle.winner = battle.results.reduce((max, curr) => {
+      return curr.result > max.result ? curr : max;
     });
-  }
+    // 4. 1등에게 걸린 모든 베팅금을 transfer한다.
+    // 5. 배틀 결과를 각 캐릭터의 배틀 히스토리에 저장한다.
+    for (let i = 0; i < battle.results.length; i++) {
+      _transferFrom(
+        battle.results[i].owner,
+        battle.winner.owner,
+        battle.betAmount
+      );
 
-  battle.winner = battle.results.reduce((max, curr) => {
-    return curr.result > max.result ? curr : max;
-  });
+      const character = _getCharacterByOwner(battle.results[i].owner);
+      character.battleHistory.push(battle.id);
+    }
 
-  // 4. 1등에게 걸린 모든 베팅금을 transfer한다.
-  // 5. 배틀 결과를 각 캐릭터의 배틀 히스토리에 저장한다.
-  for (let i = 0; i < battle.results.length; i++) {
-    _transferFrom(
-      battle.results[i].owner,
-      battle.winner.owner,
-      battle.betAmount
-    );
-
-    const character = getCharacterByOwner(battle.results[i].owner);
-    character.battleHistory.push(battle.id);
-  }
-
-  return battle;
-}
+    return battle;
+  }),
+});
